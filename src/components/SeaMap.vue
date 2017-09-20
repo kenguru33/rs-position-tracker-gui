c824718fa46b1fef06c948fc9917b23c6877e697
Bernt Anker <bernt.anker@me.com>
2017-09-20 15:47:58 +0200

    seamap impl

---
+++ src/components/SeaMap.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="google-map" :id="mapName">
+  <p>SeaMap</p>
+</div>
+</template>
+<script>
+  import GoogleMapsLoader from 'google-maps'
+  export default {
+    name: 'sea-map',
+    props: ['name'],
+    data () {
+      return {
+        mapName: this.mapName || 'seamap',
+        map: null
+      }
+    },
+    mounted: function () {
+      console.log('outside', this)
+      GoogleMapsLoader.KEY = 'AIzaSyCCFWll4oEjXml1BVeZ3-x1TZphNVx8yko'
+      GoogleMapsLoader.onLoad(function (google) {
+        console.log('I just loaded google maps api')
+      })
+      GoogleMapsLoader.load((google) => {
+        const el = document.getElementById(this.mapName)
+        const options = {
+          zoom: 10,
+          center: new google.maps.LatLng(60, 11)
+        }
+        this.map = new google.maps.Map(el, options)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+.google-map {
+  width: 800px;
+  height: 600px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import VesselsLiveView from '@/components/VesselsLiveView'
+import SeaMap from '@/components/SeaMap'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'VesselsLiveView',
       component: VesselsLiveView
+    },
+    {
+      path: '/seamap',
+      name: 'SeaMap',
+      component: SeaMap
     }
   ]
 })
