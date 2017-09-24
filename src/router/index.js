552321a3cb21ac86e71f2d40ae3d729c928e2fa1
Bernt Anker <bernt.anker@me.com>
2017-09-25 01:17:50 +0200

    cleaning up

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import VesselListView from '@/components/VesselListView'
-import VesselsLiveView from '@/components/VesselsLiveView'
 Vue.use(Router)
 
 export default new Router({
@@ -10,11 +9,6 @@ export default new Router({
       path: '/',
       name: 'VesselListView',
       component: VesselListView
-    },
-    {
-      path: '/live',
-      name: 'VesselLiveView',
-      component: VesselsLiveView
     }
   ]
 })
