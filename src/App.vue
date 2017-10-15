f12826b297ec00a6c855d6e7ee1ec008a31cbf0b
Bernt Anker <bernt.anker@me.com>
2017-10-15 15:13:25 +0200

    vessel moving halted state changing logic

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,49 @@ export default {
       }
     }
   },
+  methods: {
+    isVesselMoving: function (vessel) {
+      return vessel.SOG > 1
+    },
+    hasVesselStateChanged: function (vessel) {
+      if (!this.isVesselMoving(vessel) && this.$store.getters.movingVessels.find(t => { return t.MMSI === vessel.MMSI }) || this.isVesselMoving(vessel) && !this.$store.getters.movingVessels.find(t => { return t.MMSI === vessel.MMSI })) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
   created () {
     this.$store.dispatch('fetchVessels', 'https://ais.rs.no/aktive_pos.json')
+      .then(() => {
+        // const movingVessels = []
+        // const haltedVessels = []
+        this.$store.dispatch('setMovingVessels', [])
+        this.$store.dispatch('setHaltedVessels', [])
+        this.$store.getters.vessels.map(vessel => {
+          // this.isVesselMoving(vessel) ? movingVessels.push(vessel) : haltedVessels.push(vessel)
+          this.isVesselMoving(vessel) ? this.$store.dispatch('addMovingVessel', vessel) : this.$store.dispatch('addHaltedVessel', vessel)
+        })
+        // this.$store.dispatch('setMovingVessels', movingVessels)
+        // this.$store.dispatch('setHaltedVessels', haltedVessels)
+      })
     intervalId = setInterval(() => {
       this.$store.dispatch('fetchVessels', 'https://ais.rs.no/aktive_pos.json')
+        .then(() => {
+          this.$store.getters.vessels.map(vessel => {
+            if (this.hasVesselStateChanged(vessel)) {
+              if (this.isVesselMoving(vessel)) {
+                console.log(`add ${vessel.Ship_name} to moving vessel list`)
+                this.$store.dispatch('removeHaltedVessel', vessel)
+                this.$store.dispatch('addMovingVessel', vessel)
+              } else {
+                console.log(`add ${vessel.Ship_name} to halted vessel list`)
+                this.$store.dispatch('removeMovingVessel', vessel)
+                this.$store.dispatch('addHaltedVessel', vessel)
+              }
+            }
+          })
+        })
     }, 10000)
   },
   destroyed () {
