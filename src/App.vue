81c178c9e7f97df533b9ba47d771aa5fbf576c06
Bernt Anker <bernt.anker@me.com>
2017-10-16 10:31:11 +0200

    Vessel State change snackbar

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,17 @@
       <v-icon dark left>error</v-icon><span class="ml-2">{{ error }}</span>
       <v-btn flat class="pink--text" @click.native="snackbar = false">Close</v-btn>
     </v-snackbar>
+
+    <v-snackbar
+      :timeout="10000"
+      v-model="snackbarVesselState"
+    >
+      <v-avatar size="24px" class="mr-1">
+        <img :src="snackbarVesselImage">
+      </v-avatar>
+        <span class="ml-2">{{ vesselStateInfo }}</span>
+      <v-btn flat class="pink--text" @click.native="snackbarVesselState = false">Close</v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
@@ -27,7 +38,10 @@ export default {
   },
   data () {
     return {
-      snackbar: false
+      snackbar: false,
+      snackbarVesselState: false,
+      snackbarVesselImage: '',
+      vesselStateInfo: ''
     }
   },
   computed: {
@@ -72,10 +86,16 @@ export default {
             if (this.hasVesselStateChanged(vessel)) {
               if (this.isVesselMoving(vessel)) {
                 console.log(`add ${vessel.Ship_name} to moving vessel list`)
+                this.vesselStateInfo = `${vessel.Ship_name} is on the move!`
+                this.snackbarVesselImage = `/static/vessel-images/${vessel.MMSI}.jpg`
+                this.snackbarVesselState = true
                 this.$store.dispatch('removeHaltedVessel', vessel)
                 this.$store.dispatch('addMovingVessel', vessel)
               } else {
                 console.log(`add ${vessel.Ship_name} to halted vessel list`)
+                this.vesselStateInfo = `${vessel.Ship_name} has come to a halt!`
+                this.snackbarVesselImage = `/static/vessel-images/${vessel.MMSI}.jpg`
+                this.snackbarVesselState = true
                 this.$store.dispatch('removeMovingVessel', vessel)
                 this.$store.dispatch('addHaltedVessel', vessel)
               }
