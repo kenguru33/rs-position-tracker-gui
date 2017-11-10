b6e5196f4d78947b75f83c41089bfb66e424e2da
Bernt Anker <bernt.anker@me.com>
2017-11-11 00:36:12 +0100

    azure deploy

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -2,6 +2,6 @@ call npm install
 call npm run build
 
 call cd %DEPLOYMENT_TARGET%
-call for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\ /s /i
 echo Deployed.
