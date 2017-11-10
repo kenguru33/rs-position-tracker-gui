3fd3da598e89a32943dcdd48ac8505a3460504d2
Bernt Anker <bernt.anker@me.com>
2017-11-10 16:25:09 +0100

    change server port to 80

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
-app.listen(3000, () => console.log('listen on port 3000'))
+app.listen(80, () => console.log('listen on port 80'))
