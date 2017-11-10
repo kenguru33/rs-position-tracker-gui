167668a4f117cee3f1179686c85e2c1007c2ec7a
Bernt Anker <bernt.anker@me.com>
2017-11-11 00:21:53 +0100

    fixing azure deployment

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -4,5 +4,4 @@ call npm run build
 call cd %DEPLOYMENT_TARGET%
 call for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\ /s /i
-xcopy /d %DEPLOYMENT_SOURCE%\build\web.config %DEPLOYMENT_TARGET%\web.config
 echo Deployed.
